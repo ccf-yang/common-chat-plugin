ec58d96ba0637286f83329a7a997fbe50e16aae8
tangmu <tangmu@pplabs.org>
2025-03-11 17:04:25 +0800

    修改svg按钮大小

--- content.js
+++ content.js
@@ -180,14 +180,14 @@ function createPluginIcon() {
   const sendButton = document.createElement('button');
   sendButton.id = 'ai-plugin-send-button';
   sendButton.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <line x1="22" y1="2" x2="11" y2="13"></line>
       <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
     </svg>
   `;
   sendButton.style.cssText = `
-    width: 32px;
-    height: 32px;
+    width: 40px;
+    height: 40px;
     border-radius: 50%;
     border: none;
     background: linear-gradient(135deg, rgb(103 104 108) 0%, rgb(184, 179, 233) 100%);
